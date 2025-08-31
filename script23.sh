5f87bf072b6d3216791244317b8a3bd3fb0c781b
Tushar Sonawane <tushar.sonawane.work@gmail.com>
2025-09-01 00:55:41 +0530

    create , access array element, modify array

---
+++ script23.sh
@@ -0,0 +1,10 @@
+#Working with Arrays in Bash
+#Creating Arrays
+my_array=("value1" "value2" "value3")
+#Accessing Array Elements
+echo ${my_array[0]}
+#Modifying Array Elements
+my_array[1]="new_value"
+
+
+
