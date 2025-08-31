7073e2834bfcf347cf0b9591665ee1dde99ad06e
Tushar Sonawane <tushar.sonawane.work@gmail.com>
2025-09-01 00:08:36 +0530

    array - use store multiple value in single variable

---
+++ script8.sh
@@ -0,0 +1,10 @@
+# Array example
+fruits=("apple" "banana" "cherry")
+for fruit in "${fruits[@]}"; do
+  echo $fruit
+done
+
+
+#Arrays are used to store multiple values in a single variable. Each element in an array is accessed using an index
+
+
