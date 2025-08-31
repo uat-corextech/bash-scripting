5168e2bbd70085e7a26b71f1909c525c7c9153a0
Tushar Sonawane <tushar.sonawane.work@gmail.com>
2025-08-31 23:39:22 +0530

    basic of scripting

---
+++ script1.sh
@@ -0,0 +1,8 @@
+Basic Syntax
+
+Here are some basic rules for using Bash in scripts:
+
+Comments: Comments start with a # and Bash ignores them.
+Command Order: Commands run one after the other, from top to bottom.
+Semicolons: Use ; to run multiple commands on the same line.
+
