ff34dffb3513ed5ec9f30fdb4e0dfc213b83521e
Tushar Sonawane <tushar.sonawane.work@gmail.com>
2025-09-01 00:03:40 +0530

    add , subtract , multiply, divide

---
+++ script7.sh
@@ -0,0 +1,12 @@
+
+
+# Number example
+num1=5
+num2=10
+sum=$((num1 + num2))
+difference=$((num2 - num1))
+product=$((num1 * num2))
+quotient=$((num2 / num1))
+echo "Sum: $sum, Difference: $difference, Product: $product, Quotient: $quotient"
+
+
