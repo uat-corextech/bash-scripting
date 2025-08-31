e12a196668107024e5a673c5d00c8a2c9e96bf07
Tushar Sonawane <tushar.sonawane.work@gmail.com>
2025-09-01 00:11:34 +0530

    array utilization

--- script8.sh
+++ script8.sh
@@ -1,10 +1,13 @@
 # Array example
 fruits=("apple" "banana" "cherry")
-for fruit in "${fruits[@]}"; do
+for fruit in "${fruits[1]}"; do
   echo $fruit
 done
 
 
 #Arrays are used to store multiple values in a single variable. Each element in an array is accessed using an index
+#array index start from 0
+#@ means use all values in array
+#1 means use value at 1st index
 
 
