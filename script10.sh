9719442e6b0575c8b4bff96f19ad360ba11d681d
Tushar Sonawane <tushar.sonawane.work@gmail.com>
2025-09-01 00:18:43 +0530

    if condition usecase

---
+++ script10.sh
@@ -0,0 +1,40 @@
+Bash Operators
+Comparison Operators
+
+    -eq: Equal to
+    -ne: Not equal to
+    -lt: Less than
+    -le: Less than or equal to
+    -gt: Greater than
+    -ge: Greater than or equal to
+
+String Comparison Operators
+
+    =: Equal to
+    !=: Not equal to
+    <: Less than, in ASCII alphabetical order
+    >: Greater than, in ASCII alphabetical order
+
+Arithmetic Operators
+
+    +: Addition
+    -: Subtraction
+    *: Multiplication
+    /: Division
+    %: Modulus (remainder of division)
+    For exponentiation, use external tools like bc or awk.
+
+Logical Operators
+
+    &&: Logical AND
+    ||: Logical OR
+    !: Logical NOT
+
+File Test Operators
+
+    -e: Checks if a file exists
+    -d: Checks if a directory exists
+    -f: Checks if a file is a regular file
+    -s: Checks if a file is not empty
+
+
---
+++ script11.sh
@@ -0,0 +1,9 @@
+
+
+# Basic if statement
+num=15
+if [ $num -gt 10 ]; then
+  echo "Number is greater than 10"
+fi
+
+
