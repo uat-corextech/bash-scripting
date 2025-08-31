33c58cff1a59e2bbd57ff4c8d7ec4032802470a9
Tushar Sonawane <tushar.sonawane.work@gmail.com>
2025-09-01 00:24:56 +0530

    Elif Statements and Nested If Statements

---
+++ script13.sh
@@ -0,0 +1,13 @@
+
+
+# If...elif...else statement
+num=10
+if [ $num -gt 10 ]; then
+  echo "Number is greater than 10"
+elif [ $num -eq 10 ]; then
+  echo "Number is exactly 10"
+else
+  echo "Number is less than 10"
+fi
+
+
---
+++ script14.sh
@@ -0,0 +1,11 @@
+
+
+# Nested if statement
+num=5
+if [ $num -gt 0 ]; then
+  if [ $num -lt 10 ]; then
+    echo "Number is between 1 and 9"
+  fi
+fi
+
+
