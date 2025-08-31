e5f360e5e03fb30a6112ee5e673c9f84da6823b3
Tushar Sonawane <tushar.sonawane.work@gmail.com>
2025-09-01 00:46:51 +0530

    define and call function

---
+++ script20.sh
@@ -0,0 +1,16 @@
+#Bash Functions
+#Defining Functions
+
+#To define a function in Bash, use the following syntax. The function name is followed by parentheses, and the function body is enclosed in curly braces:
+#Example: Define a Function
+
+my_function() {
+  echo "Hello, World!"
+}
+
+#It's a good practice to use descriptive names for functions.
+
+#Calling Functions
+my_function
+
+
