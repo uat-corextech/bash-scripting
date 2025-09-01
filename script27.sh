674d8ee111685a193051373abbe41f2854646540
Tushar.sonawane <tushar@corextech.in>
2025-09-01 16:46:03 +0530

    user input using read, know to define variable name, extract variable

--- script27.sh
+++ script27.sh
@@ -2,3 +2,5 @@
 # -e  >> flag tells echo to interpret backslash escape sequences 
 # \c >> stops echo at that position
 echo -e "Hi, please type the word: \c "
+read  word
+echo "The word you entered is: $word"
