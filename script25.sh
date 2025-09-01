abfa647312c7e564fef1f63107fd2b7c8bbe9806
Tushar.sonawane <tushar@corextech.in>
2025-09-01 06:32:09 +0530

    learn case match and case execution

---
+++ script25.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+Name="Satyajit"
+case "$Name" in
+    #case 1
+    "Rajib") echo "Profession : Software Engineer" ;;
+    
+    #case 2
+    "Vikas") echo "Profession : Web Developer" ;;
+    
+    #case 3
+    "Satyajit") echo "Profession : Technical Content Writer" ;;
+esac
+
+#case $var in
+#   Pattern 1) Statement 1;;
+#   Pattern n) Statement n;;
+#esac
